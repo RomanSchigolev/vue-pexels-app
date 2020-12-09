2721330b2582920c2284f93472ae3de58d21de12
Roman Schigolev <r.shchigolev2018@yandex.ru>
2020-12-10 01:23:14 +0700

    added a file .env for the API and added a script that gets data and puts it in storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    photoList: []
+  },
+  mutations: {
+    SET_PHOTOS(state, photoList) {
+      state.photoList = photoList
+    }
+  },
+  actions: {
+    async GET_PHOTOS({ commit }) {
+      try {
+        const photoList = await axios.get("https://api.pexels.com/v1/curated?page=1&per_page=12", {
+          headers: {
+            Authorization: process.env.VUE_APP_API_KEY
+          }
+        });
+        commit("SET_PHOTOS", photoList.data.photos);
+        return photoList;
+      } catch (err) {
+        console.log(err);
+        return err;
+      }
+    }
+  },
+  getters: {
+    PHOTOS(state) {
+      return state.photoList;
+    }
+  }
 })
